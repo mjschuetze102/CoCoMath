f314f96f7bea22ae799d0dafc2fe09496575e26a
mjschuetze102 <mjs8474@rit.edu>
2018-08-01 20:22:56 -0400

    Added in the capability to add line graphs to the UI

--- src/Model/ChartDataHolder.java
+++ src/Model/ChartDataHolder.java
@@ -8,7 +8,11 @@ import java.util.Observable;
  */
 public class ChartDataHolder extends Observable {
 
+    // Variable to hold the statistics for the UI to use
     private ArrayList<Double> stats;
+
+    // Variable to hold values for the line charts to use
+    private ArrayList<ArrayList<Double>> cmcSpecificStats;
 
     /////////////////////////////////////////////////////
     //              Class Functionality
@@ -21,12 +25,39 @@ public class ChartDataHolder extends Observable {
      * @param threeDrops- number of three drops in the deck
      */
     public void computeStatistics(int oneDrops, int twoDrops, int threeDrops) {
-        if (oneDrops != -1 && twoDrops != -1 && threeDrops != -1)
-           stats = Statistics.compute(oneDrops, twoDrops, threeDrops);
+        if (oneDrops != -1 && twoDrops != -1 && threeDrops != -1) {
+            // Initialize the CMC Specific Stats variable
+            cmcSpecificStats = new ArrayList<>();
 
-        // Notify observers that a change has occurred
-        setChanged();
-        notifyObservers();
+            // Store drop counts in an easily callable way from a loop
+            int[] dropCount = new int[] {oneDrops, twoDrops, threeDrops};
+
+            // Compute the CMC value being pulled from CoCo for changes in each CMC value input
+            for (int cmc = 0; cmc < 3; cmc++) {
+                // Initialize the ArrayList for the specific CMC value
+                cmcSpecificStats.add(new ArrayList<>());
+
+                // Compute stats for each input +- 5
+                for (int index = dropCount[cmc] - 5; index < dropCount[cmc] + 6; index++) {
+                    // Create a copy of the original drop counts so it's not edited
+                    int[] tempArray = new int[3];
+                    System.arraycopy(dropCount, 0, tempArray, 0, 3);
+
+                    // Change the value of the chosen CMC value input
+                    tempArray[cmc] = index;
+
+                    // Add the statistic to the list
+                    cmcSpecificStats.get(cmc).add(Statistics.compute(tempArray[0], tempArray[1], tempArray[2]).get(5));
+                }
+            }
+
+            // Compute the stats for UI output
+            stats = Statistics.compute(oneDrops, twoDrops, threeDrops);
+
+            // Notify observers that a change has occurred
+            setChanged();
+            notifyObservers();
+        }
     }
 
     /////////////////////////////////////////////////////
@@ -39,6 +70,15 @@ public class ChartDataHolder extends Observable {
      */
     public ArrayList<Double> getStats() {
         return this.stats;
+    }
+
+    /**
+     * Provides the statistics data for the specified cmc value
+     * @param cmc- the CMC for the input that's being changed
+     * @return ArrayList of the values that would appear if the amount of drops for the chosen CMC was changed
+     */
+    public ArrayList<Double> getCMCSpecificStats(int cmc) {
+        return this.cmcSpecificStats.get(cmc);
     }
 
     /////////////////////////////////////////////////////
---
+++ src/View/MainPage/LineGraphDisplay.java
@@ -0,0 +1,87 @@
+package View.MainPage;
+
+import Model.ChartDataHolder;
+import javafx.geometry.Pos;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.StackPane;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Written by Michael Schuetze on 8/1/2018.
+ */
+public class LineGraphDisplay extends StackPane implements Observer {
+
+    // Chart used to show output to the user
+    private LineChart<Number, Number> chart;
+
+    // CMC this graph will represent
+    private int cmc;
+
+    LineGraphDisplay(ChartDataHolder backend, int cmc) {
+        backend.addObserver(this);
+        this.cmc = cmc;
+        start();
+    }
+
+    /////////////////////////////////////////////////////
+    //              Class Functionality
+    /////////////////////////////////////////////////////
+
+    /**
+     * Create the UI elements that will display the line chart to the user
+     */
+    private void start () {
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+
+        chart = new LineChart<>(xAxis, yAxis);
+        chart.setTitle("Changes in " + cmc + " Drops");
+        chart.setMaxWidth(300);
+
+        this.getChildren().add(chart);
+        this.setAlignment(Pos.CENTER_LEFT);
+    }
+
+    /**
+     * Receives data from the backend and updates the display
+     * @param o- the object that was changed
+     * @param arg- varies based on object
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o instanceof ChartDataHolder)
+            displayStatistics(((ChartDataHolder) o).getCMCSpecificStats(cmc - 1));
+    }
+
+    /////////////////////////////////////////////////////
+    //              Getters and Setters
+    /////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////
+    //               Helper Functions
+    /////////////////////////////////////////////////////
+
+    /**
+     * Displays the output to the user
+     * @param stats- statistics calculated from user input
+     */
+    private void displayStatistics(ArrayList<Double> stats) {
+        XYChart.Series series = new XYChart.Series();
+        series.setName("CMC");
+
+        for (int index = 0; index < stats.size(); index++) {
+            series.getData().add(new XYChart.Data(index, stats.get(index)));
+        }
+
+        chart.getData().add(series);
+    }
+
+    /////////////////////////////////////////////////////
+    //               Testing Purposes
+    /////////////////////////////////////////////////////
+}
